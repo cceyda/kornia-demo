7678e77fe5c022eceda13210741c9c99c8a0efe8
Ceyda Cinarel <15624271+cceyda@users.noreply.github.com>
2021-04-20 20:17:30 +0900

    Update kornia_aug.py

--- kornia_aug.py
+++ kornia_aug.py
@@ -24,7 +24,7 @@ if uploaded_file is not None:
     im = Image.open(uploaded_file)
 else:
     im = Image.open("./images/pretty_bird.jpg")
-scaler = int(im.height / 1.8)
+scaler = int(im.height / 2)
 st.sidebar.image(im, caption="Input Image", width=256)
 image = F.pil_to_tensor(im).float() / 255
 
